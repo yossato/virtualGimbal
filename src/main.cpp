140a3b40f0744aa16f7cf81f8ba7a0c0b17996f0
Yoshiaki Sato <sato.y.ba@gmail.com>
2017-03-30 23:21:30 +0900

    三角形が写った

--- src/main.cpp
+++ src/main.cpp
@@ -238,42 +238,9 @@ int main(int argc, char** argv){
         // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
         // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
         static const GLfloat g_vertex_buffer_data[] = {
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-             1.0f, 1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-             1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-             1.0f,-1.0f,-1.0f,
-             1.0f, 1.0f,-1.0f,
-             1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-             1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-             1.0f,-1.0f, 1.0f,
-             1.0f, 1.0f, 1.0f,
-             1.0f,-1.0f,-1.0f,
-             1.0f, 1.0f,-1.0f,
-             1.0f,-1.0f,-1.0f,
-             1.0f, 1.0f, 1.0f,
-             1.0f,-1.0f, 1.0f,
-             1.0f, 1.0f, 1.0f,
-             1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-             1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-             1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-             1.0f,-1.0f, 1.0f
+            -1.0f, -1.0f, 0.0f,
+             1.0f, -1.0f, 0.0f,
+             0.0f,  1.0f, 0.0f,
         };
 
         // Two UV coordinatesfor each vertex. They were created with Blender.
@@ -281,39 +248,7 @@ int main(int argc, char** argv){
             0.000059f, 0.000004f,
             0.000103f, 0.336048f,
             0.335973f, 0.335903f,
-            1.000023f, 0.000013f,
-            0.667979f, 0.335851f,
-            0.999958f, 0.336064f,
-            0.667979f, 0.335851f,
-            0.336024f, 0.671877f,
-            0.667969f, 0.671889f,
-            1.000023f, 0.000013f,
-            0.668104f, 0.000013f,
-            0.667979f, 0.335851f,
-            0.000059f, 0.000004f,
-            0.335973f, 0.335903f,
-            0.336098f, 0.000071f,
-            0.667979f, 0.335851f,
-            0.335973f, 0.335903f,
-            0.336024f, 0.671877f,
-            1.000004f, 0.671847f,
-            0.999958f, 0.336064f,
-            0.667979f, 0.335851f,
-            0.668104f, 0.000013f,
-            0.335973f, 0.335903f,
-            0.667979f, 0.335851f,
-            0.335973f, 0.335903f,
-            0.668104f, 0.000013f,
-            0.336098f, 0.000071f,
-            0.000103f, 0.336048f,
-            0.000004f, 0.671870f,
-            0.336024f, 0.671877f,
-            0.000103f, 0.336048f,
-            0.336024f, 0.671877f,
-            0.335973f, 0.335903f,
-            0.667969f, 0.671889f,
-            1.000004f, 0.671847f,
-            0.667979f, 0.335851f
+
         };
 
         GLuint vertexbuffer;
@@ -335,12 +270,12 @@ int main(int argc, char** argv){
             glUseProgram(programID);
 
             // Compute the MVP matrix from keyboard and mouse input
-            computeMatricesFromInputs();
-            glm::mat4 ProjectionMatrix = getProjectionMatrix();
-            glm::mat4 ViewMatrix = getViewMatrix();
-            glm::mat4 ModelMatrix = glm::mat4(1.0);
-            glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
-
+//            computeMatricesFromInputs();
+//            glm::mat4 ProjectionMatrix = getProjectionMatrix();
+//            glm::mat4 ViewMatrix = getViewMatrix();
+//            glm::mat4 ModelMatrix = glm::mat4(1.0);
+//            glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+            glm::mat4 MVP = glm::mat4(1.0f);
             // Send our transformation to the currently bound shader,
             // in the "MVP" uniform
             glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
