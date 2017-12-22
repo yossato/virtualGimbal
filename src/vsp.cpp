1112736b7dd29af4dd5a018060a3d44cce83f058
Yoshiaki Sato <virtualgimbal@xa2.so-net.ne.jp>
2017-12-22 23:42:51 +0900

    Modified vsp class

--- src/vsp.cpp
+++ src/vsp.cpp
@@ -8,9 +8,11 @@ vsp::vsp()
 //template <class T> vsp::vsp(vector<quaternion<T>> &angle_quaternion)
 Eigen::Quaternion<double> vsp::RotationQuaternion(double theta, Eigen::Vector3d n){
     //nを規格化する
-    double tmp = 1.0/sqrt(n[0]*n[0]+n[1]*n[1]+n[2]*n[2]);
-    n = n * tmp;
-    return Eigen::Quaternion<double>(cos(theta/2),n[0]*sin(theta/2),n[1]*sin(theta/2),n[2]*sin(theta/2));
+//    double tmp = 1.0/sqrt(n[0]*n[0]+n[1]*n[1]+n[2]*n[2]);
+//    n = n * tmp;
+    auto n_sin_theta_2 = n.normalized()*sin(theta/2.0);
+//    return Eigen::Quaternion<double>(cos(theta/2),n[0]*sin(theta/2),n[1]*sin(theta/2),n[2]*sin(theta/2));
+    return Eigen::Quaternion<double>(cos(theta/2),n_sin_theta_2[0],n_sin_theta_2[1],n_sin_theta_2[2]);
 }
 
 /**
@@ -18,12 +20,12 @@ Eigen::Quaternion<double> vsp::RotationQuaternion(double theta, Eigen::Vector3d 
  **/
 
 Eigen::Quaternion<double> vsp::RotationQuaternion(Eigen::Vector3d w){
-    double theta = sqrt(w[0]*w[0]+w[1]*w[1]+w[2]*w[2]);	//!<回転角
-    if(theta == 0){
+    double theta = w.norm();//sqrt(w[0]*w[0]+w[1]*w[1]+w[2]*w[2]);	//!<回転角
+    if(theta == 0.0){
         return Eigen::Quaternion<double>(1,0,0,0);
     }
-    auto n = w*(1.0/theta);								//!<回転軸を表す単位ベクトル
-    return RotationQuaternion(theta, n);
+//    auto n = w*(1.0/theta);								//!<回転軸を表す単位ベクトル
+    return RotationQuaternion(theta, w.normalized());
 }
 
 vector<double> vsp::getRow(int r){
@@ -61,9 +63,9 @@ Eigen::Quaternion<double> vsp::toRawQuaternion(uint32_t frame){
     //0割を回避するためにマクローリン展開
     //Order: w, x, y, z.
     if(theta > EPS){
-        auto n = w * (1.0/theta);//単位ベクトルに変換
+        Eigen::VectorXd n = w.normalized();//w * (1.0/theta);//単位ベクトルに変換
 //        double sin_theta_2 = sin(theta*0.5);
-        auto n_sin_theta_2 = n * sin(theta*0.5);
+        Eigen::VectorXd n_sin_theta_2 = n * sin(theta*0.5);
         return Eigen::Quaternion<double>(cos(theta*0.5),n_sin_theta_2[0],n_sin_theta_2[1],n_sin_theta_2[2]);
     }else{
         return Eigen::Quaternion<double>(1.0,0.5*w[0],0.5*w[1],0.5*w[2]);
@@ -77,9 +79,9 @@ Eigen::Quaternion<double> vsp::toFilteredQuaternion(uint32_t frame){
     //0割を回避するためにマクローリン展開
     //Order: w, x, y, z.
     if(theta > EPS){
-        auto n = w * (1.0/theta);//単位ベクトルに変換
+        Eigen::VectorXd n = w.normalized();//w * (1.0/theta);//単位ベクトルに変換
 //        double sin_theta_2 = sin(theta*0.5);
-        auto n_sin_theta_2 = n * sin(theta*0.5);
+        Eigen::VectorXd n_sin_theta_2 = n * sin(theta*0.5);
         return Eigen::Quaternion<double>(cos(theta*0.5),n_sin_theta_2[0],n_sin_theta_2[1],n_sin_theta_2[2]);
     }else{
         return Eigen::Quaternion<double>(1.0,0.5*w[0],0.5*w[1],0.5*w[2]);
