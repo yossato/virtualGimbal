b4b4325c73d1b7db556579e5be44f57347ab586d
Yoshiaki Sato <virtualgimbal@xa2.so-net.ne.jp>
2018-04-04 00:45:18 +0900

    Delete unimportant part

--- src/mainsyncandstabilize.cpp
+++ src/mainsyncandstabilize.cpp
@@ -314,43 +314,8 @@ int main(int argc, char** argv){
     int32_t angular_velocity_length_in_video = ceil(angular_velocity_from_csv.size() * Tav / Tvideo);
     int32_t lengthDiff = angular_velocity_length_in_video - estimatedAngularVelocity.size();
     cout << "lengthDiff:" << lengthDiff << endl;
-//    vector<double> correlationCoefficients(lengthDiff);
-//    double minCC = DBL_MAX;
-//    for(int32_t offset = 0; offset < lengthDiff; offset++){
-//        double sum = 0.0;
-//        for(int32_t i=0; i<estimatedAngularVelocity.size();i++){
-//            sum +=   abs(angularVelocity(i+offset)[0]-estimatedAngularVelocity[i][0])
-//                    + abs(angularVelocity(i+offset)[1]-estimatedAngularVelocity[i][1])
-//                    + abs(angularVelocity(i+offset)[2]-estimatedAngularVelocity[i][2]);
-//            if(sum > minCC){
-//                break;
-//            }
-//        }
-//        if(sum < minCC){
-//            minCC = sum;
-//        }
-//        correlationCoefficients[offset] = sum;
-//    }
-
-    //最小となる要素を取得
-//    int32_t minPosition = std::distance(correlationCoefficients.begin(),min_element(correlationCoefficients.begin(),correlationCoefficients.end()));
-
-//    //正しくサブピクセル推定するために、最小となった要素の次の要素を計算し直す
-//    {
-//        double sum = 0.0;
-//        for(int32_t i=0; i<estimatedAngularVelocity.size();i++){
-//            sum +=   abs(angularVelocity(i+minPosition+1)[0]-estimatedAngularVelocity[i][0])
-//                    + abs(angularVelocity(i+minPosition+1)[1]-estimatedAngularVelocity[i][1])
-//                    + abs(angularVelocity(i+minPosition+1)[2]-estimatedAngularVelocity[i][2]);
-//        }
-//        correlationCoefficients[minPosition+1] = sum;
-//    }
 
     t2 = std::chrono::system_clock::now() ;
-//    // 処理の経過時間
-//    elapsed = t2 - t1 ;
-//    std::cout << "Elapsed time@search minimum: " << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() << " ms" << std::endl;
-
 
     //ここでEigenのMatrixでできるだけ計算するルーチンを追加してみる
     Eigen::MatrixXd angular_velocity_matrix             = Eigen::MatrixXd::Zero(ceil(angular_velocity_from_csv.size()*Tav /Tvideo),3);
@@ -388,23 +353,6 @@ int32_t min_position = std::distance(correlation_coefficients.begin(),min_elemen
     cout << "subframe minposition :" << min_position+subframeOffset << endl;
 
     if(debug_signal_processing) show_correlation(angular_velocity_from_csv,estimatedAngularVelocity, Tvideo,Tav,min_position+subframeOffset);
-
-    //同期が取れている角速度を出力する関数を定義
-//    auto angularVelocitySync = [&angular_velocity_from_csv, Tvideo, Tav, min_position, subframeOffset](int32_t frame){
-//        //        double dframe = (frame + min_position + subframeOffset) * Tav / Tvideo;
-//        double dframe = (frame + min_position + subframeOffset) * Tvideo / Tav;
-//        int i = floor(dframe);
-//        double decimalPart = dframe - (double)i;
-//        //領域外にはみ出した時は、末端の値で埋める
-//        if(i<0){
-//            return angular_velocity_from_csv[0];
-//        }else if(angular_velocity_from_csv.size()<=(i+1)){
-//            return angular_velocity_from_csv.back();
-//        }else{
-//            Eigen::Vector3d retval = angular_velocity_from_csv[i]*(1.0-decimalPart)+angular_velocity_from_csv[i+1]*decimalPart;
-//            return retval;
-//        }
-//    };
 
     auto convCVMat2EigenMat = [](cv::Mat &src){
         assert(src.type()==CV_64FC1);
@@ -498,15 +446,6 @@ int32_t min_position = std::distance(correlation_coefficients.begin(),min_elemen
         return 0;
     }
 
-    //vspクラスによる平滑化波形の生成
-//    vector<Eigen::Quaternion<double>> angleQuaternion_vsp2;
-//    angleQuaternion_vsp2.push_back(Eigen::Quaternion<double>(1,0,0,0));
-//    for(int frame= -1 ,e=Capture->get(CV_CAP_PROP_FRAME_COUNT)+1;frame<e;++frame){//球面線形補間を考慮し前後各1フレーム追加
-//        auto v_sync = angularVelocitySync(frame);
-//        Eigen::Vector3d ve_sync(v_sync[0],v_sync[1],v_sync[2]);
-//        angleQuaternion_vsp2.push_back(angleQuaternion_vsp2.back()*vsp::RotationQuaternion(ve_sync*Tvideo));
-//        angleQuaternion_vsp2.back() = angleQuaternion_vsp2.back().normalized();
-//    }
 
     vsp v2(/*angleQuaternion_vsp2,*/
            division_x,
@@ -527,211 +466,8 @@ int32_t min_position = std::distance(correlation_coefficients.begin(),min_elemen
     v2.filteredDataDFT(Capture->get(CV_CAP_PROP_FPS),1.0);//TODO:引数修正。もはやあまり意味がない。
     v2.filteredQuaternion(100,Capture->get(CV_CAP_PROP_FPS),1.0);
 
-//    cv::Mat buff(textureSize.height,textureSize.width,CV_8UC3);//テクスチャ用Matを準備
-
     v2.init_opengl(textureSize);
 
-    /*
-    // Initialise GLFW
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        getchar();
-        return -1;
-    }
-
-#define TEST2D
-
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef TEST2D
-    glfwWindowHint( GLFW_VISIBLE, 0 );//オフスクリーンレンダリング。
-#endif
-
-    // Open a window and create its OpenGL context
-
-#ifndef TEST2D
-    window = glfwCreateWindow( 1920, 1080, "Tutorial 0 - Keyboard and Mouse", glfwGetPrimaryMonitor(), NULL);
-#else
-    window = glfwCreateWindow( 1920, 1080, "Tutorial 0 - Keyboard and Mouse", NULL, NULL);
-#endif
-    if( window == NULL ){
-        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // Initialize GLEW
-    glewExperimental = true; // Needed for core profile
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        getchar();
-        glfwTerminate();
-        return -1;
-    }
-#ifndef TEST2D
-    glfwIconifyWindow(window);
-#endif
-    //    glfwHideWindow(window);
-
-    // Ensure we can capture the escape key being pressed below
-    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-    // Hide the mouse and enable unlimited mouvement
-    //        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    // Set the mouse at the center of the screen
-    glfwPollEvents();
-    //    glfwSetCursorPos(window, 1024/2, 768/2);
-
-    // Dark blue background
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-    // Enable depth test
-    glEnable(GL_DEPTH_TEST);
-    // Accept fragment if it closer to the camera than the former one
-    glDepthFunc(GL_LESS);
-
-    // Cull triangles which normal is not towards the camera
-    glEnable(GL_CULL_FACE);
-
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-
-    // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader" );
-
-    // Get a handle for our "MVP" uniform
-    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-
-
-#ifndef TEST2D
-    // Create one OpenGL texture
-    GLuint textureID_0;
-    glGenTextures(1, &textureID_0);
-    //OpenGLに「これから、テクスチャ識別子idに対して指示を与えます」と指示
-    glBindTexture(GL_TEXTURE_2D,textureID_0);
-    //テクスチャをここで作成
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,buff.cols,buff.rows,0,GL_BGR,GL_UNSIGNED_BYTE,buff.data);
-#else
-    // The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
-    GLuint FramebufferName = 0;
-    glGenFramebuffers(1, &FramebufferName);
-    glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-
-    // The texture we're going to render to
-    GLuint renderedTexture;
-
-    GLuint textures[2];
-    glGenTextures(2, textures);
-    renderedTexture = textures[0];
-    GLuint textureID_0 = textures[1];
-
-    glBindTexture(GL_TEXTURE_2D,textureID_0);
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,buff.cols,buff.rows,0,GL_BGR,GL_UNSIGNED_BYTE,buff.data);
-#endif
-
-
-    static const GLfloat border[] = { 0.0, 0.0, 0.0, 0.0 };//背景色
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border);//テクスチャの境界色
-    //テクスチャの繰り返しの設定
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-#ifdef TEST2D
-    // "Bind" the newly created texture : all future texture functions will modify this texture
-    glBindTexture(GL_TEXTURE_2D, renderedTexture);
-
-    // Give an empty image to OpenGL ( the last "0" means "empty" )
-    glTexImage2D(GL_TEXTURE_2D, 0,GL_RGB, imageSize.width, imageSize.height, 0,GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-    // Poor filtering
-    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-    // Set "renderedTexture" as our colour attachement #0
-    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
-
-    // Set the list of draw buffers.
-    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-    glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-
-    // Always check that our framebuffer is ok
-    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE){
-        return false;
-    }
-#endif
-
-    // Get a handle for our "myTextureSampler" uniform
-    GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
-
-
-
-    std::vector<GLfloat> vecTexture;
-    for(int j=0;j<division_y;++j){							//jは終了の判定が"<"であることに注意
-        double v	= (double)j/division_y*imageSize.height;
-        double v1	= (double)(j+1)/division_y*imageSize.height;
-        for(int i=0;i<division_x;++i){
-            double u	= (double)i/division_x*imageSize.width;
-            double u1	= (double)(i+1)/division_x*imageSize.width;
-            //OpenGL側へ送信するテクスチャの頂点座標を準備
-            vecTexture.push_back((GLfloat)u/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v/textureSize.height);//y座標
-            vecTexture.push_back((GLfloat)u1/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v/textureSize.height);//y座標
-            vecTexture.push_back((GLfloat)u/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v1/textureSize.height);//y座標
-
-
-            vecTexture.push_back((GLfloat)u/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v1/textureSize.height);//y座標
-            vecTexture.push_back((GLfloat)u1/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v/textureSize.height);//y座標
-            vecTexture.push_back((GLfloat)u1/textureSize.width);//x座標
-            vecTexture.push_back((GLfloat)v1/textureSize.height);//y座標
-
-
-        }
-    }
-
-    std::vector<GLfloat> vecVtx(vecTexture.size());					//頂点座標
-
-    GLuint vertexbuffer;
-    glGenBuffers(1, &vertexbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    //    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, vecVtx.size()*sizeof(GLfloat), vecVtx.data(), GL_DYNAMIC_DRAW);
-
-    GLuint uvbuffer;
-    glGenBuffers(1, &uvbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    //    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, vecTexture.size()*sizeof(GLfloat), vecTexture.data(), GL_STATIC_DRAW);
-
-    //歪補正の準備
-    GLuint nFxyID       = glGetUniformLocation(programID, "normalizedFocalLength");
-    GLuint nCxyID       = glGetUniformLocation(programID, "normalizedOpticalCenter");
-    GLuint distCoeffID  = glGetUniformLocation(programID, "distortionCoeffs");
-
-
-    //動画の位置を修正
-    cv::Mat img;
-*/
     //一度動画を閉じて、seek可能版に置き換える
     int32_t e=Capture->get(CV_CAP_PROP_FRAME_COUNT);
     delete Capture;
@@ -740,138 +476,17 @@ int32_t min_position = std::distance(correlation_coefficients.begin(),min_elemen
     cv::namedWindow("Preview",cv::WINDOW_NORMAL);
 
     for(int32_t i=0;i<e;++i){
-cv::Mat simg;
+        cv::Mat simg;
         if(0 != v2.spin_once(i,sCapture,simg)){
             break;
         }
-        /*v2.getDistortUnrollingMapQuaternion(i,vecVtx);
-
-#ifdef TEST2D
-        // Render to our framebuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-        glViewport(0,0,imageSize.width,imageSize.height); // Render on the whole framebuffer, complete from the lower left corner to the upper right
-#endif
-
-        // Clear the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Use our shader
-        glUseProgram(programID);
-
-
-        glm::mat4 MVP = glm::mat4(1.0f);//動画保存用
-        // Send our transformation to the currently bound shader,
-        // in the "MVP" uniform
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glBufferData(GL_ARRAY_BUFFER, vecVtx.size()*sizeof(GLfloat), vecVtx.data(),GL_DYNAMIC_DRAW);
-
-#if MULTITHREAD_CAPTURE
-        {
-            std::lock_guard<std::mutex> lock(mtvc.mtx);
-            img = mtvc.images.front().clone();
-            mtvc.images.pop_front();//先頭を削除
-        }
-#else
-        sCapture.getFrame(i,img);
-#endif
-
-#ifdef TEST2D
-        // Bind our texture in Texture Unit 0
-        glActiveTexture(GL_TEXTURE0);
-#endif
-
-        // Bind our texture in Texture Unit 0
-        //        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, textureID_0);//            glBindTexture(GL_TEXTURE_2D, Texture);
-        glTexSubImage2D(GL_TEXTURE_2D,0,0,0,img.cols,img.rows,GL_BGR,GL_UNSIGNED_BYTE,img.data);
-        //        glGenerateMipmap(GL_TEXTURE_2D);
-        // Set our "myTextureSampler" sampler to user Texture Unit 0
-        glUniform1i(TextureID, 0);
-
-        //歪補正の準備
-        float nfxy[] = {(float)(matIntrinsic.at<double>(0,0)/imageSize.width), (float)(matIntrinsic.at<double>(1,1)/imageSize.height)};
-        glUniform2fv(nFxyID, 1, nfxy);
-        float ncxy[] = {(float)(matIntrinsic.at<double>(0,2)/imageSize.width), (float)(matIntrinsic.at<double>(1,2)/imageSize.height)};
-        glUniform2fv(nCxyID, 1, ncxy);
-        //        float distcoeffFloat[] = {(float)(matDist.at<double>(0,0)),(float)(matDist.at<double>(0,1)),(float)(matDist.at<double>(0,2)),(float)(matDist.at<double>(0,3))};
-        float distcoeffFloat[] = {(float)(matInvDistort.at<double>(0,0)),(float)(matInvDistort.at<double>(0,1)),(float)(matInvDistort.at<double>(0,2)),(float)(matInvDistort.at<double>(0,3))};
-        glUniform4fv(distCoeffID, 1, distcoeffFloat);
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-                    0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-                    2,                  // size
-                    GL_FLOAT,           // type
-                    GL_FALSE,           // normalized?
-                    0,                  // stride
-                    (void*)0            // array buffer offset
-                    );
-
-        // 2nd attribute buffer : UVs
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(
-                    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-                    2,                                // size : U+V => 2
-                    GL_FLOAT,                         // type
-                    GL_FALSE,                         // normalized?
-                    0,                                // stride
-                    (void*)0                          // array buffer offset
-                    );
-
-        // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, vecVtx.size()*2); // 12*3 indices starting at 0 -> 12 triangles
-
-#if 1
-        ////////////////////
-        cv::Mat simg(imageSize,CV_8UC3);
-        //~ glReadBuffer(GL_FRONT);//読み取るOpenGLのバッファを指定 GL_FRONT:フロントバッファ GL_BACK:バックバッファ
-
-
-        //        glReadBuffer(GL_BACK);//読み取るOpenGLのバッファを指定 GL_FRONT:フロントバッファ GL_BACK:バックバッファ
-        // OpenGLで画面に描画されている内容をバッファに格納
-        glReadPixels(
-                    0,					//読み取る領域の左下隅のx座標
-                    0,					//読み取る領域の左下隅のy座標 //0 or getCurrentWidth() - 1
-                    imageSize.width,				//読み取る領域の幅
-                    imageSize.height,				//読み取る領域の高さ
-                    GL_BGR,				//it means GL_BGR,           //取得したい色情報の形式
-                    GL_UNSIGNED_BYTE,	//読み取ったデータを保存する配列の型
-                    simg.data			//ビットマップのピクセルデータ（実際にはバイト配列）へのポインタ
-                    );
-#else
-        cv::Mat simg(textureSize,CV_8UC3);
-        glGetTexImage(GL_TEXTURE_2D,0,GL_BGR,GL_UNSIGNED_BYTE,simg.data);
-#endif
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
-
-        cv::imshow("Stabilized Image2",simg);
-        char key =cv::waitKey(1);*/
 
         if(outputStabilizedVideo){
             std::lock_guard<std::mutex> lock(buffer.mtx);
             buffer.images.push_back(cv::Mat());
             buffer.images.back() = simg.clone();
         }
-/*
 
-        /////////////////////
-
-        // Swap buffers
-        //        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        if(glfwGetKey(window, GLFW_KEY_ESCAPE ) == GLFW_PRESS ||
-                glfwWindowShouldClose(window) != 0 ){
-            break;
-        }*/
 
         //Show fps
         auto t4 = std::chrono::system_clock::now();
@@ -887,22 +502,9 @@ cv::Mat simg;
         fflush(stdout);
 
 
-    } // Check if the ESC key was pressed or the window was closed
-    //    while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-    //           glfwWindowShouldClose(window) == 0 );
+    }
 
     v2.stop_opengl();
-    /*
-    // Cleanup VBO and shader
-    glDeleteBuffers(1, &vertexbuffer);
-    glDeleteBuffers(1, &uvbuffer);
-    glDeleteProgram(programID);
-    glDeleteTextures(1, &TextureID);
-    glDeleteVertexArrays(1, &VertexArrayID);
-    //ここでTextureID_0をDeleteしなくて大丈夫？
-    // Close OpenGL window and terminate GLFW
-    glfwTerminate();
-    */
 
     //動画書き出しのマルチスレッド処理の終了処理
     //書き込みが終わっているか確認
