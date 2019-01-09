b5bba4f3ad5b5e4545d14082cf924ba1fca6abe0
Yoshiaki Sato <virtualgimbal@xa2.so-net.ne.jp>
2019-01-10 07:12:27 +0900

    post_processing_software: Modified rapidjson_test.cpp

--- post_processing_software/src/rapidjson_test.cpp
+++ post_processing_software/src/rapidjson_test.cpp
@@ -2,9 +2,12 @@
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
+
+#include "rapidjson/filereadstream.h"
+
 #include <iostream>
 using namespace rapidjson;
-int main() {
+int main(int argc, char *argv[]) {
     // 1. Parse a JSON string into DOM.
     const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
     Document d;
@@ -18,5 +21,31 @@ int main() {
     d.Accept(writer);
     // Output {"project":"rapidjson","stars":11}
     std::cout << buffer.GetString() << std::endl;
+
+    if (argc < 2) return 0;
+
+    FILE* fp = fopen(argv[1], "rb"); // non-Windows use "r"
+    char readBuffer[65536];
+    FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+    Document e;
+    e.ParseStream(is);
+    fclose(fp);
+
+    static const char* kTypeNames[] =
+        { "Null", "False", "True", "Object", "Array", "String", "Number" };
+
+    for(auto &m: e.GetObject()){
+        printf("Type of menber %s is %s\n",m.name.GetString(), kTypeNames[m.value.GetType()]);
+    }
+
+    const Value& angular_velocity_rad_per_sec = e["angular_velocity_rad_per_sec"];
+    for(auto& v: angular_velocity_rad_per_sec.GetArray()){
+        printf("{\n");
+        for(auto &z: v.GetArray()){
+            printf("%f\n",z.GetDouble());
+        }
+        printf("}\n");
+    }
+
     return 0;
 }
