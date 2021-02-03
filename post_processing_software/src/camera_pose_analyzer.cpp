2ee0fa9a94adda1e0874bbb53203f24ce80ed531
Yoshiaki Sato <virtualgimbal@xa2.so-net.ne.jp>
2021-02-04 07:10:43 +0900

    Clean up

--- post_processing_software/src/camera_pose_analyzer.cpp
+++ post_processing_software/src/camera_pose_analyzer.cpp
@@ -84,74 +84,39 @@ int main(int argc, char **argv)
         }
     }
 
-    // cv::namedWindow("inpaint",cv::WINDOW_NORMAL);
-    // cv::namedWindow("colored_map",cv::WINDOW_NORMAL);
-    // cv::namedWindow("colored_map_0",cv::WINDOW_NORMAL);
-    // cv::namedWindow("colored_map_1",cv::WINDOW_NORMAL);
-    // cv::namedWindow("colored_map_2",cv::WINDOW_NORMAL);
-
     cv::namedWindow("frame2", cv::WINDOW_NORMAL);
     cv::namedWindow("source",cv::WINDOW_NORMAL);
 
     // Read video frame
     cv::VideoCapture cap(videoPass);
-    cv::UMat umat_src,umat_next;
+    cv::UMat umat_old,umat_next;
+
+    float resize_ratio = 1./16.;
 
     // Read a first frame
-    cap >> umat_src;
+    cap >> umat_old;
+    cv::resize(umat_old,umat_old,cv::Size(),resize_ratio,resize_ratio,cv::INTER_AREA);
     // Read a next frame
     cap >> umat_next;
+    cv::resize(umat_next,umat_next,cv::Size(),resize_ratio,resize_ratio,cv::INTER_AREA);
+
+    cv::UMat mono_old,mono_next;
+    cv::Mat float_old,float_next;
+    cv::cvtColor(umat_old,mono_old,cv::COLOR_BGRA2GRAY);
+    mono_old.convertTo(float_old,CV_32F);
+    
     while(!umat_next.empty())
     {
 
-        cv::UMat mini_color;
-        cv::resize(umat_src,mini_color,cv::Size(),1./16.,1./16.,cv::INTER_AREA);
-        cv::imshow("source",mini_color);
-
-        cv::UMat mono_latest;
-        cv::UMat mono_next;
-        cv::cvtColor(umat_src,mono_latest,cv::COLOR_BGRA2GRAY);
-        cv::cvtColor(umat_next,mono_next,cv::COLOR_BGRA2GRAY);
-        cv::Mat float_latest,float_next;
-        mono_latest.convertTo(float_latest,CV_32F);
-        mono_next.convertTo(float_next,CV_32F);
-        // cv::Size map_size = cv::Size(map_col,map_row);
-        // cv::Size window_size = cv::Size(window_width,window_height);
-        // cv::Mat map = calculateOpticalFlow(map_size,window_size,float_latest,float_next);
+        cv::imshow("source",umat_old);
         
-        // cv::Mat visualize_image = umat_src.getMat(cv::ACCESS_RW).clone();
-        // visualizeInpaintingMap(visualize_image,window_size,map);
-        // cv::resize(visualize_image,visualize_image,cv::Size(),0.3,0.3);
-        // cv::imshow("inpaint",visualize_image);
-
-        // cv::Mat colored_map = generateOpticalFlowMap(map,colored_map_gain);
-        // cv::imshow("colored_map",colored_map);
-
-        // cv::Mat pyramid_float_latest = float_latest.clone();
-        // cv::Mat pyramid_float_next = float_next.clone();
-        // cv::Size pyramid_map_size = map_size;
-        // std::vector<cv::Mat> pyramid_colored_map;
-        // double pyramid_colored_map_gain = colored_map_gain;
-        // for(int i=0;i<5;++i)
-        // {
-        //     cv::resize(pyramid_float_latest,pyramid_float_latest,pyramid_float_latest.size()/2,0,0,cv::INTER_AREA);
-        //     cv::resize(pyramid_float_next,pyramid_float_next,pyramid_float_next.size()/2,0,0,cv::INTER_AREA);
-        //     pyramid_map_size = pyramid_map_size/2;
-        //     pyramid_colored_map_gain *= 2.;
-        //     if(pyramid_float_latest.cols < window_size.width) break;
-        //     if(pyramid_float_latest.rows < window_size.height) break;
-
-        //     cv::Mat pyramid_map = calculateOpticalFlow(pyramid_map_size,window_size,pyramid_float_latest,pyramid_float_next);
-        //     cv::Mat pyramid_colored_map = generateOpticalFlowMap(pyramid_map,pyramid_colored_map_gain);
-        //     cv::imshow(std::string("colored_map_")+std::to_string(i),pyramid_colored_map); 
-        // }
+        cv::cvtColor(umat_next,mono_next,cv::COLOR_BGRA2GRAY);
+        mono_next.convertTo(float_next,CV_32F);
+ 
 
         {
-            cv::Mat flow(float_latest.size(), CV_32FC2);
-            cv::Mat mini_float_latest,mini_float_next;
-            cv::resize(float_latest,mini_float_latest,cv::Size(),1./16.,1./16.,cv::INTER_AREA);
-            cv::resize(float_next,mini_float_next,cv::Size(),1./16.,1./16.,cv::INTER_AREA);
-            cv::calcOpticalFlowFarneback(mini_float_latest, mini_float_next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+            cv::Mat flow(float_old.size(), CV_32FC2);
+            cv::calcOpticalFlowFarneback(float_old, float_next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
             // visualization
             cv::Mat flow_parts[2];
             cv::split(flow, flow_parts);
@@ -170,37 +135,7 @@ int main(int argc, char **argv)
             cv::imshow("frame2", bgr);
         }
 
-        /*{
-            // build pyramids
-            std::vector<cv::Mat> vec_pyramid_latest,vec_pyramid_next;
-            vec_pyramid_latest.push_back(float_latest.clone());
-            vec_pyramid_next.push_back(float_next.clone());
-            for(int i=0;i<5;++i)
-            {
-                cv::Mat lastest = vec_pyramid_latest.back();
-                cv::Mat next = vec_pyramid_next.back();
-
-                if(lastest.size().width % 2
-                || lastest.size().height % 2
-                || lastest.size().width < window_size.width
-                || lastest.size().height < window_size.height)
-                {
-                    break;
-                }
-
-                vec_pyramid_next.push_back(cv::Mat());
-                vec_pyramid_next.push_back(cv::Mat());
-                cv::resize(lastest,vec_pyramid_latest.back(),cv::Size(),0.5,0.5,cv::INTER_AREA);
-                cv::resize(next,vec_pyramid_next.back(),cv::Size(),0.5,0.5,cv::INTER_AREA);
-                
-            }
-            // Calculate optical flow from the pyramids
-            for(int i=vec_pyramid_latest.size()-1;i>=0;++i)
-            {
-                cv::Mat optical_flow = calculateOpticalFlow(pyramid_map_size,window_size,pyramid_float_latest,pyramid_float_next);
-            }
-
-        }*/
+       
 
         uint8_t key = (uint8_t)cv::waitKey(1);
         if(key == 'q')
@@ -217,8 +152,11 @@ int main(int argc, char **argv)
                 break;
             }
         }
-        umat_src = std::move(umat_next);
+        umat_old = std::move(umat_next);
+        mono_old = std::move(mono_next);
+        float_old = std::move(float_next);
         cap >> umat_next;
+        cv::resize(umat_next,umat_next,cv::Size(),resize_ratio,resize_ratio,cv::INTER_AREA);
     }
     return EXIT_SUCCESS;
 }
